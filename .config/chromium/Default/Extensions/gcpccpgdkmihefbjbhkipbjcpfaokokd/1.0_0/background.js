224fe413603eb903e326c8dd5a8f6e9741970b4a
Danil Semelenov <mail@danil.mobi>
2019-06-17 14:03:41 +0300

    Allow to close the last tab

--- .config/chromium/Default/Extensions/gcpccpgdkmihefbjbhkipbjcpfaokokd/1.0_0/background.js
+++ .config/chromium/Default/Extensions/gcpccpgdkmihefbjbhkipbjcpfaokokd/1.0_0/background.js
@@ -13,9 +13,9 @@ chrome.commands.onCommand.addListener(command =>
   chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) =>
     command === 'close-tab'
       ? chrome.tabs.query({ windowId: tab.windowId }, tabs =>
-          tabs.length > 1
+          tabs.length > 1 || tab.url === 'chrome://newtab/'
             ? chrome.tabs.remove(tab.id)
-            : chrome.tabs.update(tab.id, { url: 'chrome://newtab' })
+            : chrome.tabs.update(tab.id, { url: 'chrome://newtab/' })
         )
       : command === 'copy-title-url'
       ? copy([tab.title, tab.url].filter(Boolean).join(' '))
