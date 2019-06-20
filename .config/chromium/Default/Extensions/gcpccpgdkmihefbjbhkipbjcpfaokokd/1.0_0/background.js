589615f5a718df2bf11efc3e431cfe9a2b93bffc
Danil Semelenov <mail@danil.mobi>
2019-06-21 00:12:00 +0300

    Close popup windows in one step, split code into functions

--- .config/chromium/Default/Extensions/gcpccpgdkmihefbjbhkipbjcpfaokokd/1.0_0/background.js
+++ .config/chromium/Default/Extensions/gcpccpgdkmihefbjbhkipbjcpfaokokd/1.0_0/background.js
@@ -1,4 +1,11 @@
-const copy = text => {
+const closeTab = tab =>
+  chrome.tabs.query({ windowId: tab.windowId, windowType: 'normal' }, tabs =>
+    tabs.length > 1 || !tabs.length || tab.url === 'chrome://newtab/'
+      ? chrome.tabs.remove(tab.id)
+      : chrome.tabs.update(tab.id, { url: 'chrome://newtab/' })
+  );
+
+const copyText = text => {
   addEventListener(
     'copy',
     event => {
@@ -9,20 +16,23 @@ const copy = text => {
   );
   document.execCommand('copy');
 };
-chrome.commands.onCommand.addListener(command =>
-  chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) =>
-    command === 'close-tab'
-      ? chrome.tabs.query({ windowId: tab.windowId }, tabs =>
-          tabs.length > 1 || tab.url === 'chrome://newtab/'
-            ? chrome.tabs.remove(tab.id)
-            : chrome.tabs.update(tab.id, { url: 'chrome://newtab/' })
-        )
-      : command === 'copy-title-url'
-      ? copy([tab.title, tab.url].filter(Boolean).join(' '))
-      : command === 'copy-url'
-      ? copy(tab.url)
-      : command === 'toggle-pinned'
-      ? chrome.tabs.update(null, { pinned: !tab.pinned })
-      : undefined
-  )
-);
+
+const main = () =>
+  chrome.commands.onCommand.addListener(command =>
+    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) =>
+      command === 'close-tab'
+        ? closeTab(tab)
+        : command === 'copy-title-url'
+        ? copyText([tab.title, tab.url].filter(Boolean).join(' '))
+        : command === 'copy-url'
+        ? copyText(tab.url)
+        : command === 'toggle-pinned'
+        ? togglePinnedTab(tab)
+        : undefined
+    )
+  );
+
+const togglePinnedTab = tab =>
+  chrome.tabs.update(null, { pinned: !tab.pinned });
+
+main();
