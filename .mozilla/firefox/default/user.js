5509461896ea47935154fb0b97bc16a21297c8e9
Danil Semelenov <mail@danil.mobi>
2020-04-27 22:54:55 -0400

    Add a preference

--- .mozilla/firefox/default/user.js
+++ .mozilla/firefox/default/user.js
@@ -12,6 +12,7 @@ user_pref("browser.tabs.warnOnClose", false);
 user_pref("browser.uiCustomization.state", "{\"currentVersion\":16,\"placements\":{\"TabsToolbar\":[\"tabbrowser-tabs\",\"alltabs-button\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"urlbar-container\"]},\"seen\":[\"developer-button\",\"ublock0_raymondhill_net-browser-action\"]}");
 user_pref("browser.urlbar.suggest.searches", true);
 user_pref("extensions.pocket.enabled", false);
+user_pref("extensions.privatebrowsing.notification", true);
 user_pref("general.smoothScroll", false);
 user_pref("general.warnOnAboutConfig", false);
 user_pref("privacy.donottrackheader.enabled", true);
